d8c555c5bda701a7ca247ccbbc2e2f05e630fa66
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-03-14 11:17:47 -0600

    Create HT5

---
+++ HT5.py
@@ -0,0 +1,18 @@
+# Universidad del Valle de Guatemala
+# Hoja de Trabajo 5
+# Algoritmos y Estructuras de Datos
+# Moises Alonso
+# Maria Marta Ramirez Gil 21342
+
+'''Importar las clases necesarias'''
+import random
+import simpy
+import time
+
+'''Crear la variable de inicio'''
+inicio = time.time()
+
+def Run(name,env,CPUrunning,CPUOfComputer):
+    global TimeTotal
+
+
