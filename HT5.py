21fc64963c600379d2396efc3d46deb8aeea9faf
María Marta Ramírez Gil <ram21342@uvg.edu.gt>
2022-03-14 11:22:11 -0600

    Incisos a y b
    Aplicacion de los incisos a y b de la hoja de trabajo

--- HT5.py
+++ HT5.py
@@ -14,5 +14,33 @@ inicio = time.time()
 
 def Run(name,env,CPUrunning,CPUOfComputer):
     global TimeTotal
+    yield env.timeout(CPUrunning)
+    
+    ending = env.now
+    
+    TimeProgram = random.randint(1, 10)
+    print ('%s llega a las %f necesita %d instrucciones para salir del CPU' % (name,ending,TimeProgram))
+    
+    with CPUOfComputer.request() as turn:
+        
+        yield turn
+        yield env.timeout(TimeProgram)
+        print ('%s sale del CPU a las %f' % (name, env.now))
+        
+    tiempoTotal = env.now - ending
+    print ('%s se tardo %f' % (name, tiempoTotal))
+    TimeTotal = TimeTotal + tiempoTotal
+    
+env = simpy.Environment() 
+CPUOfComputer = simpy.Resource(env,capacity = 3) 
+random.seed(10)
 
+TimeTotal = 0
+for i in range(25):
 
+    env.process(Run('Programa %d'%i,env,random.expovariate(1.0/10),CPUOfComputer))
+
+env.run(until=500)  
+
+fin = time.time()
+print(fin-inicio)
